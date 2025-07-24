c3a77717843fca91b3778cdfb753a05cb9fccd70
Tarun Pal <143593638+TarunPal0812@users.noreply.github.com>
2025-07-24 11:48:41 +0530

    Update backend URL initialization in AdminContext and DoctorContext to provide a default value

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -6,7 +6,9 @@ import { toast } from "react-toastify";
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl =
+    import.meta.env.VITE_BACKEND_URL ||
+    "https://docappoint-server.onrender.com";
   const [aToken, setAToken] = useState(
     localStorage.getItem("atoken") ? localStorage.getItem("atoken") : ""
   );
